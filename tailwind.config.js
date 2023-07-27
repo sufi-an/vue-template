4c07932206ebfa7eff7e287cf2ab9d5d80be79f1
Fah1m09 <mahmudurfahim@gmail.com>
2023-07-27 13:33:29 +0600

    design changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        btnGreen: '#55B07C',
+        btnRed: '#E85F5C',
+        lightStone: '#F0F0F0',
+        fillGrey: '#F4F6F9',
+        lightBlack: '#B1B0C0',
+        testPurple: 'rgb(145, 85, 253)',
+        testGrey: '#777777',
+        deepGreen: '#187979',
+        error: '#FF6262',
+        popupBG: '#F4F6F9',
+      },
+    },
   },
   plugins: [],
+  darkMode: "class"
 };
